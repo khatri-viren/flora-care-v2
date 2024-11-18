7b6940efe53c241899990235a32606798211f961
Viren Khatri <vk102002@gmail.com>
2024-11-18 12:16:41 +0530

    fix: correct server port and address
    backend/index.js: Changes the server port to 3000 and listens on all interfaces using '0.0.0.0'. This ensures the server is accessible from other devices on the network. The previous port, 4000 was incorrect and restricted accessibility.

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,6 @@ config();
 import { server } from "./src/server.js";
 import mqttClient from "./src/mqtt/client.js";
 
-server.listen(process.env.PORT || 4000, () => {
+server.listen(process.env.PORT || 3000, "0.0.0.0", () => {
   console.log(`Server is running on port ${process.env.PORT || 4000}`);
 });
