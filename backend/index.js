e8613a9d87e90b4d77454cfd501dd56c8ebda2a8
Viren Khatri <vk102002@gmail.com>
2025-05-02 01:33:42 +0530

    ✨(index.js): Config environment port.
    feat: Configure the server to listen on the environment port.
    * index.js: Added configuration for the server to listen on the
     environment port specified in 'process.env.PORT' or default to 3000.

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ config();
 import { server } from "./src/server.js";
 import mqttClient from "./src/mqtt/client.js";
 
+// some change
 server.listen(process.env.PORT || 3000, "0.0.0.0", () => {
   console.log(`Server is running on port ${process.env.PORT || 4000}`);
 });
